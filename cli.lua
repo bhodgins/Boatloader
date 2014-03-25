f68a1e8f3c8b21fd709914f192acf8f35c512c6f
vifino <adrian@Adrians-MacBook-Pro.local>
2014-03-25 20:24:04 +0100

    Added CLI.lua

---
+++ cli.lua
@@ -0,0 +1,22 @@
+local function cli(commands)
+	local running = true
+	while running do
+		local input = read()
+		command = {}
+		for word in input:gmatch("%S+") do table.insert(commands, word) end
+		if not (commands[1] == "quit") then
+			local shellArgs
+			for item in commands do
+				if not item == 1 then
+					
+				else
+					table.insert(commands[item])
+				end
+			end
+			local currentFunction = command[commands[1]]
+			currentFunction[shellArgs]
+		else
+			
+		end
+	end
+end
