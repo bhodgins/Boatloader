372cb67883558f3ddf56c516ae3af2a0784644ae
Brian Hodgins <bhodgins@9600-baud.net>
2014-03-26 18:00:05 -0400

    added some verbosity to build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@ echo "" > bios.lua
 
 # The order here is critical!:
 declare -a SRCFILES=('basic.lua' 'clifunc.lua' 'boot.lua' 'cli.lua')
+echo "building..."
 for file in ${SRCFILES[@]}
 do
 	cat ${file} >> bios.lua
 	echo -e "\n" >> bios.lua
+	echo ${file}
 done
