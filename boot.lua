a8b36fb2608d1969b2c94604d6d6aa4b6e5c012e
Brian Hodgins <bhodgins@9600-baud.net>
2014-03-26 17:08:03 -0400

    Boot options

--- boot.lua
+++ boot.lua
@@ -1,19 +1,47 @@
 -- boot.lua
 -- This file does the OS loading
 
-local kernel
-local osRoot
+local bootImage
+local bootParams
 
 function boot() -- Takes no arguments, this is the start of the booting Process.
-	local osConf
+   local osConf
+   local u = {}
+
+   if not bootImage then
+      print("No image specified")
+      return false
+   end
+   
+   -- Make a copy of _G:
+   for k, v in pairs(_G) do u[k] = v end
+   local newEnv = setmetatable(u, getmetatable(_G))
+   
+   -- TODO: patch fs API?
+   
+   local funFile = error = loadfile(kernel)
+   if funFile then
+      local env = _environment ; from env.lua
+      setfenv( funFile, env)
+      local success, error = pcall(function() funFile(unpack(args)) end)
+      if not success then
+	 if error and error ~= "" then
+	    printError(error)
+	 end
+	 return false
+      end
+      return false
+   else
+      print("No image specified")
+   end
 end
 
-function kernel(arg) -- Set the kernel
-	kernal = arg[1]
+function image(arg) -- Set the boot image / kernel
+   bootImage = arg[1]
 end
 
-function setRoot(arg) -- Sets the Root, the Config will be in /.osconf ( <-- Where you can put stuff, you can execute before loading the OS )
-	osRoot=arg[1]
+function append(arg) -- passes params to the kernel
+   bootParams = arg[1]
 end
 
 -- End boot.lua
